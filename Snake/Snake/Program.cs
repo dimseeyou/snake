f2580466ac90a7652a251c2e3508ab52cc503040
dimseeyou <gubkabob85@mail.ru>
2015-12-24 14:59:51 +0300

    add function

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -10,10 +10,30 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int x1 = 3;
+            int y1 = 3;
+            char symb1 = '*';
+
+            Draw(x1, y1, symb1);
+
+            int x2 = 4;
+            int y2 = 5;
+
+            char symb2 = '&';
+
+            Draw(x2, y2, symb2);
+
+
             Console.ReadLine();
 
+            
+        }
 
+        static void Draw(int x, int y, char symb)
+        {
+
+            Console.SetCursorPosition(x, y);
+            Console.Write(symb);
         }
     }
 }
